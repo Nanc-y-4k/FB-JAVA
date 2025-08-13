51f4fa0f9474b78d253eae87862268f772e47212
NANCY 2415001011 <n007ancyjaiswal@gmail.com>
2025-08-13 13:57:10 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int first_no = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int second_no = sc.nextInt();
+        System.out.println(first_no + second_no); // Addition of two integers
+    }
+}
+
+
