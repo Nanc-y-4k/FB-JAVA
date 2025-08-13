0fb94af9d50a1214b2d5f71c5f76ffb8a867e308
NANCY 2415001011 <n007ancyjaiswal@gmail.com>
2025-08-13 09:24:10 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class main { // main is file name
+  public static void main(String[] args) {
+    System.out.println("hello world");
+    // single line comment : by double slash
+
+    /*
+     * multi-line comment : by slash and asterisk
+     */
+
+    System.out.println("nancy jaisawal");
+    System.out.println("2ndyear");
+    System.out.println("2415001011");
+    // FOR output in new line use println in java
+
+  }
+}
